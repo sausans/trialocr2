fb86deefc37308cf70d8ec88d0f369c130cca26c
Aliyah Sausan Huwel <ahuwel@Aliyahs-MacBook-Air.local>
2024-06-16 17:41:29 -0400

    move packages to main

--- myenv/trial_ocr2.py
+++ myenv/trial_ocr2.py
@@ -11,6 +11,19 @@ if tesseract_path:
 else:
     st.error("Tesseract not found. Please ensure it's installed and on your PATH.")
 
+def process_image_and_generate_drafts(image):
+    try:
+        extracted_text = pytesseract.image_to_string(image, lang='eng')
+    except Exception as e:
+        st.warning(f"pytesseract failed with error: {e}. Using tesserocr instead.")
+        extracted_text = tesserocr.image_to_text(image)
+        
+    context = extracted_text.strip()
+    #original_embedding = get_embedding(context)
+    #rec_docs = query_pinecone(original_embedding)
+    #draft_responses = chain_of_thought_prompting(context, rec_docs)
+
+    return context#, draft_responses
 
 def load_image(image_file):
     return Image.open(image_file)
@@ -23,7 +36,7 @@ def main():
         image = load_image(image_file)
         st.image(image, caption='Uploaded Image', use_column_width=True)
         if st.button("Recognize Text"):
-            result_text = pytesseract.image_to_string(image, lang='eng')
+            result_text = process_image_and_generate_drafts(image)
             st.write(result_text)
 
 if __name__ == "__main__":
